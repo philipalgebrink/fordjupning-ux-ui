48cfced5f4818d4455ede92200dbd2bb3d5de410
Philip Älgebrink <76400643+philipalgebrink@users.noreply.github.com>
2024-10-18 18:12:25 +0200

    Finished Loading screen
    * Added simple animation to slogan
    * Changed background color
    * Changed font size
    * Changed text color

--- interval-app/src/App.vue
+++ interval-app/src/App.vue
@@ -3,3 +3,11 @@
     <router-view />
   </div>
 </template>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+</style>
--- interval-app/src/components/Loading.vue
+++ interval-app/src/components/Loading.vue
@@ -6,17 +6,30 @@
       class="logo"
       @click="goToSetTimer"
     />
-    <p class="slogan">For all your timing needs</p>
+    <p class="slogan" ref="sloganText">For all your timing needs</p>
   </div>
 </template>
 
 <script>
+import anime from "animejs/lib/anime.es.js";
+
 export default {
   name: "LoadingScreen", // Ändra komponentnamn här
   methods: {
     goToSetTimer() {
       this.$router.push("/set-timer");
     },
+  },
+
+  mounted() {
+    anime({
+      targets: this.$refs.sloganText,
+      scale: [1, 1.1], // Ändra storlek (För att ge "pulserande" effekt)
+      easing: "easeInOutQuad",
+      duration: 1500, // Per loop
+      loop: true, // Loop
+      direction: "alternate", // Så den går fram o tillbaka
+    });
   },
 };
 </script>
@@ -28,17 +41,17 @@ export default {
   justify-content: center;
   align-items: center;
   height: 100vh;
-  background-color: #f0f0f0;
+  width: 100vw;
+  background-color: #000000;
 }
 
 .logo {
-  width: 150px;
-  cursor: pointer;
+  width: 50%;
 }
 
 .slogan {
   margin-top: 20px;
-  font-size: 18px;
-  color: #333;
+  font-size: 20px;
+  color: #ffffff;
 }
 </style>
