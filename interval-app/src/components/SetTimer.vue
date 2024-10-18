bbd91ad8daa0c68dc4dd877f4f45bdcb54f9cbbe
Philip Älgebrink <76400643+philipalgebrink@users.noreply.github.com>
2024-10-18 19:02:06 +0200

    Finished StartTimer Design
    * Design for StartTimer page finished
    * Added input for minutes
    * Added start timer button (will work with actual timer later on)
    * Added checks for input of minutes
    * Designed according to mockup

--- interval-app/src/components/SetTimer.vue
+++ interval-app/src/components/SetTimer.vue
@@ -1,13 +1,175 @@
 <template>
-  <div>
-    <h1>Set Timer</h1>
+  <div class="set-timer-screen">
+    <div class="menu-icon">
+      <img src="@/assets/navicon.svg" alt="Menu Icon" @click="openMenu" />
+    </div>
+
+    <div class="timer-settings">
+      <button @click="decreaseTime">‹</button>
+      <input
+        type="number"
+        v-model="time"
+        class="time-display"
+        min="1"
+        max="999"
+        @blur="validateTime"
+      />
+      <button @click="increaseTime">›</button>
+    </div>
+    <p class="minutes-label">minutes</p>
+
+    <!-- Ny container för checkboxar och knapp -->
+    <div class="options-and-button">
+      <div class="interval-options">
+        <label>
+          <input type="checkbox" v-model="intervals" />
+          intervals (VG)
+        </label>
+        <label :style="{ visibility: intervals ? 'visible' : 'hidden' }">
+          <input type="checkbox" v-model="breaks" />
+          5 min break / interval (VG)
+        </label>
+      </div>
+
+      <button class="start-button" @click="startTimer">START TIMER</button>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "SetTimer",
+  data() {
+    return {
+      time: 10,
+      intervals: false,
+      breaks: false,
+    };
+  },
+  methods: {
+    increaseTime() {
+      if (this.time < 999) {
+        this.time++;
+      }
+    },
+    decreaseTime() {
+      if (this.time > 1) {
+        this.time--;
+      }
+    },
+    validateTime() {
+      if (this.time > 999) {
+        this.time = 999;
+      } else if (this.time < 1 || isNaN(this.time)) {
+        this.time = 1;
+      }
+    },
+    startTimer() {
+      console.log("Timer started with:", this.time, "minutes");
+    },
+    openMenu() {
+      console.log("Menu opened");
+    },
+  },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.set-timer-screen {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  width: 100vw;
+  background-color: #f5f5f5;
+  position: relative;
+}
+
+.menu-icon {
+  position: absolute;
+  top: 20px;
+  left: 20px;
+}
+
+.menu-icon img {
+  width: 30px;
+  height: 30px;
+  cursor: pointer;
+}
+
+.timer-settings {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.time-display {
+  font-size: 72px;
+  font-weight: bold;
+  margin: 0 40px;
+  text-align: center;
+  width: 135px;
+  border: none;
+  background: none;
+  appearance: none;
+}
+
+.timer-settings button {
+  font-size: 48px;
+  background: none;
+  border: none;
+  cursor: pointer;
+  font-weight: bold;
+  padding: 0 10px;
+}
+
+.minutes-label {
+  font-size: 16px;
+  color: #555;
+  text-align: center;
+}
+
+/* Ny container för checkboxar och startknapp */
+.options-and-button {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start; /* Säkerställ vänsterjustering */
+  width: 100%;
+  max-width: 250px;
+  margin-top: 68px;
+}
+
+.interval-options {
+  margin-bottom: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start; /* Vänsterjustera checkboxarna */
+}
+
+.interval-options label {
+  display: flex;
+  align-items: center;
+  margin-bottom: 15px;
+  font-size: 18px;
+}
+
+input[type="checkbox"] {
+  margin-right: 10px;
+  width: 20px;
+  height: 20px;
+  border: 2px solid #000;
+  cursor: pointer;
+}
+
+.start-button {
+  padding: 15px 40px;
+  font-size: 20px;
+  border: 2px solid #000;
+  background-color: transparent;
+  cursor: pointer;
+  display: inline-block;
+  align-self: flex-start;
+  width: 100%;
+}
+</style>
