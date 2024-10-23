e50098368002ed52dca60cfda125a5c4b297a205
Philip Älgebrink <76400643+philipalgebrink@users.noreply.github.com>
2024-10-23 19:42:43 +0200

    Added comments
    * Easier explain how it works

--- interval-app/src/utils/numToWords.js
+++ interval-app/src/utils/numToWords.js
@@ -35,11 +35,13 @@ export function numToWords(num) {
   ];
 
   if (num < 20) {
+    // Om numret är under 20 så använder vi units för ja ARTON osv.
     return units[num];
   } else if (num < 100) {
-    const ten = Math.floor(num / 10);
-    const unit = num % 10;
-    return unit === 0
+    // Om numret är över 20 men under 100 använder vi tens för tjugo, trettio osv.
+    const ten = Math.floor(num / 10); // Tar det första siffran av talet (tiotalet). Till exempel, för 42 blir detta 4.
+    const unit = num % 10; // Tar resten av divisionen, dvs entalet (isåfall 2 om man går på exemplet ovan).
+    return unit === 0 // Kollar om talet är 0 och isf returnerar vi endast fyrtio
       ? tens[ten]
       : `${tens[ten]}${units[unit] !== "noll" ? ` ${units[unit]}` : ""}`;
   } else {
